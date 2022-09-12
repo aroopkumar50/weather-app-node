9c3b47d0b54df5b29865b54d8c55324cc6c97be1
Aroop Kumar <aroopkumar50@gmail.com>
2022-09-12 23:46:34 +0530

    Add dotenv configuration

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const request = require("request");
 
 const geocode = (address, callback) => {
